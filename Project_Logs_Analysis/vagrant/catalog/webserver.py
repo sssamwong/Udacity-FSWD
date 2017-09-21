e083f8a1a289aa65a2dbc80d442fc437bb13a1d9
Wong SS <siuswong6@gmail.com>
2017-09-21 15:15:47 +0800

    Enable user to add new restaurants in webserver.py

--- Project_Logs_Analysis/vagrant/catalog/webserver.py
+++ Project_Logs_Analysis/vagrant/catalog/webserver.py
@@ -14,6 +14,13 @@ class list_all_restaurant(object):
     def return_names(self):
         return self.RestaurantNames
 
+class add_restaurant(object):
+    def __init__(self, new_restaurant_name):
+        self.new_restaurant_name = new_restaurant_name
+        newRestaurant = Restaurant(name=self.new_restaurant_name[0])
+        session.add(newRestaurant)
+        session.commit()
+
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
@@ -23,6 +30,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
+                output += "<a href = '/restaurants/new'>Make a New Restaurant Here</a><br><br>"
                 lister = list_all_restaurant()
                 RestaurantNames = lister.return_names()
                 for RestaurantName in RestaurantNames:
@@ -30,6 +38,22 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<br>"
                     output += "<a href = '#'>Edit</a><br>"
                     output += "<a href = '#'>Delete</a><br>"
+                    output += "<br><br>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print (output)
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1> Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/restaurants/new'>"  # noqa
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name'>"  # noqa
+                output += "<input type = 'submit' value='Create'>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 print (output)
@@ -66,22 +90,35 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('newRestaurantName')
 
-                output = ""
-                output += "<html><body>"
-                output += " <h2> Okay, how about this: </h2>"
-                output += "<h1> %s </h1>" % messagecontent[0]
+                # Adding the new restaurant name
+                add_restaurant(messagecontent)
 
-                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name = 'message' type = 'text' ><input type = 'submit' value = 'Submit'> </form>"  # noqa
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+                return
+
+#            self.send_response(301)
+#            self.end_headers()
+#            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
+#            if ctype == 'multipart/form-data':
+#                fields = cgi.parse_multipart(self.rfile, pdict)
+#                messagecontent = fields.get('message')
+#                output = ""
+#                output += "<html><body>"
+#                output += " <h2> Okay, how about this: </h2>"
+#                output += "<h1> %s </h1>" % messagecontent[0]
+#                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name = 'message' type = 'text' ><input type = 'submit' value = 'Submit'> </form>"  # noqa
+#                output += "</body></html>"
+#                self.wfile.write(output)
+#                print output
 
         except Exception as e:
             pass
@@ -91,7 +128,6 @@ def main():
         port = 8080
         server = HTTPServer(('', port), webserverHandler)
         print ("Web server running on port %s" % port)
-        print (list_all_restaurant())
         server.serve_forever()
 
     except KeyboardInterrupt:
