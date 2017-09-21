ad2f8a7111245cebc6389065a5858cc6cdfe5bd9
Wong SS <siuswong6@gmail.com>
2017-09-21 20:47:04 +0800

    Replacing the for loop with split for do_GET and do_POST

--- Project_Logs_Analysis/vagrant/catalog/webserver.py
+++ Project_Logs_Analysis/vagrant/catalog/webserver.py
@@ -8,7 +8,6 @@ engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
-restaurant_list = session.query(Restaurant).all()
 
 class rename_restaurant(object):
     def __init__(self, newName, id_number):
@@ -65,9 +64,10 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print (output)
                 return
 
-            for RestaurantName in restaurant_list:
-                id_number = RestaurantName.id
-                if self.path.endswith("/restaurants/%s/edit" % id_number):
+            if self.path.endswith("/edit"):
+                id_number = self.path.split("/")[2]
+                RestaurantName = session.query(Restaurant).filter_by(id=id_number).one()
+                if RestaurantName != []:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
@@ -82,34 +82,8 @@ class webserverHandler(BaseHTTPRequestHandler):
                     print (output)
                     return
 
-            if self.path.endswith("/hello"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "Hello!"
-                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name = 'message' type = 'text' ><input type = 'submit' value = 'Submit'> </form>"  # noqa
-                output += "</body></html>"
-                self.wfile.write(output)
-                print (output)
-                return
-
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "&#161Hola <a href = '/hello' >Back to Hello</a>"
-                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name = 'message' type = 'text' ><input type = 'submit' value = 'Submit'> </form>"  # noqa
-                output += "</body></html>"  # noqa
-                self.wfile.write(output)
-                print (output)
-                return
         except IOError:
-            self.send_error(404, "File Not Found %s" % self.path)
-
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
@@ -128,37 +102,21 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 return
 
-            for RestaurantName in restaurant_list:
-                id_number = RestaurantName.id
-                if self.path.endswith("/restaurants/%s/edit" % id_number):
-                    ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
-                    if ctype == 'multipart/form-data':
-                        fields = cgi.parse_multipart(self.rfile, pdict)
-                    newName = fields.get('renameRestaurantName')
+            if self.path.endswith("/edit"):
+                id_number = self.path.split("/")[2]
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                newName = fields.get('renameRestaurantName')
 
-                    # Adding the new restaurant name
-                    rename_restaurant(newName, id_number)
+                # Adding the new restaurant name
+                rename_restaurant(newName, id_number)
 
-                    self.send_response(301)
-                    self.send_header('Content-type', 'text/html')
-                    self.send_header('Location', '/restaurants')
-                    self.end_headers()
-                    return
-
-#            self.send_response(301)
-#            self.end_headers()
-#            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))  # noqa
-#            if ctype == 'multipart/form-data':
-#                fields = cgi.parse_multipart(self.rfile, pdict)
-#                messagecontent = fields.get('message')
-#                output = ""
-#                output += "<html><body>"
-#                output += " <h2> Okay, how about this: </h2>"
-#                output += "<h1> %s </h1>" % messagecontent[0]
-#                output += "<form method = 'POST' enctype = 'multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name = 'message' type = 'text' ><input type = 'submit' value = 'Submit'> </form>"  # noqa
-#                output += "</body></html>"
-#                self.wfile.write(output)
-#                print output
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+                return
 
         except Exception as e:
             pass
