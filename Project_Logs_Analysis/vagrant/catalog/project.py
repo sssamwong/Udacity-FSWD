8361af06c4bb9fc3b6ab0cc40251935ccafc8f05
Wong SS <siuswong6@gmail.com>
2017-09-23 11:13:47 +0800

    Updating the edit function in url

--- Project_Logs_Analysis/vagrant/catalog/project.py
+++ Project_Logs_Analysis/vagrant/catalog/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,9 +19,15 @@ def restaurantMenu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id=restaurant_id)  # noqa
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 #    newitem = session.query(Restaurant).filter_by(id=restaurant_id).one()
 
 # Task 2: Create route for editMenuItem function here
