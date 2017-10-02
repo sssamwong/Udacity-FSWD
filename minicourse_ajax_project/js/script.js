ab242c006cb42a8b3a3d28944f4313d3aa6abd60
Wong SS <siuswong6@gmail.com>
2017-10-02 23:54:37 +0800

    Loading the user-defined streetview to the background

--- minicourse_ajax_project/js/script.js
+++ minicourse_ajax_project/js/script.js
@@ -6,12 +6,15 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $streetStr = $('#street').val();
+    var $cityStr = $('#city').val();
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $streetStr + ',' + $cityStr + '">');
 
     // YOUR CODE GOES HERE!
 
