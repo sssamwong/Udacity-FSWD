ffb15361c5d129079b3f9c693a3f1dea244895af
Wong SS <siuswong6@gmail.com>
2017-10-03 20:56:14 +0800

    Cosmetic change on the background code

--- minicourse_ajax_project/js/script.js
+++ minicourse_ajax_project/js/script.js
@@ -6,15 +6,21 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-    var $streetStr = $('#street').val();
-    var $cityStr = $('#city').val();
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+
     // load streetview
-    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $streetStr + ',' + $cityStr + '">');
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // YOUR CODE GOES HERE!
 
