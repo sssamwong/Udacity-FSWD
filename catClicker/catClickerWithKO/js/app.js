8b091692ed0628c32f833e07b6ba985953737967
Wong SS <siuswong6@gmail.com>
2017-10-17 07:23:53 +0800

    Adding the nickname array using forearh control flow

--- catClicker/catClickerWithKO/js/app.js
+++ catClicker/catClickerWithKO/js/app.js
@@ -2,6 +2,13 @@ var ViewModel = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.nicknames = ko.observableArray([
+        {nickname: "Tab"},
+        {nickname: "Tablap"},
+        {nickname: "T"},
+        {nickname: "TT"},
+        {nickname: "Ta"}
+        ]);
 
     this.incrementCounter = function(){
         this.clickCount(this.clickCount() + 1);
