0e59c6e829ac3de33d858df14470f91863822943
Wong SS <siuswong6@gmail.com>
2017-10-16 23:30:36 +0800

    Adding the cat name, click counter and image into the web using knockout

---
+++ catClicker/catClickerWithKO/js/app.js
@@ -0,0 +1,11 @@
+var ViewModel = function(){
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+
+    this.incrementCounter = function(){
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings(new ViewModel())
