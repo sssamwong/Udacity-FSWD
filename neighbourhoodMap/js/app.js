46de537b70c53ea60c1835c933860c250bbcd895
Wong SS <siuswong6@gmail.com>
2017-11-04 23:32:21 +0800

    Adding the top navigation bar which includes the hamburger icon

--- neighbourhoodMap/js/app.js
+++ neighbourhoodMap/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('.sidebarBtn').click(function(){
 		$('.sidebar').toggleClass('active');
 		$('.sidebarBtn').toggleClass('toggle');
+		$('#map').toggleClass('comeOut');
 	})
 })
 
