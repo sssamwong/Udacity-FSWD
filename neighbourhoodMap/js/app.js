abbcb25172cbc0cdcdc74d2122495f3965edcdea
Wong SS <siuswong6@gmail.com>
2017-11-04 11:55:11 +0800

    Adding hamburger menu and icon

--- neighbourhoodMap/js/app.js
+++ neighbourhoodMap/js/app.js
@@ -1,8 +1,15 @@
+$(document).ready(function(){
+	$('.sidebarBtn').click(function(){
+		$('.sidebar').toggleClass('active');
+		$('.sidebarBtn').toggleClass('toggle');
+	})
+})
+
 var map;
 function initMap() {
 	// Constructor creates a new map
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 22.2860793, lng: 114.2111942},
-		zoom: 10
+		center: {lat: 22.2817824, lng: 114.1567264},
+		zoom: 12
 	});
 }
