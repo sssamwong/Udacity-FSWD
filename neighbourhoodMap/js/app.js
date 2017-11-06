1ac8d8a957a748f22cc05864a4d778e39916dd1d
Wong SS <siuswong6@gmail.com>
2017-11-07 00:30:45 +0800

    Adding bounce animation to the marker when the list view is clicked

--- neighbourhoodMap/js/app.js
+++ neighbourhoodMap/js/app.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 // Default point of interests
 
 var initialPOI = [
-	{localLanguage: '雲南小鍋米線', title: 'Wannam Siu Wok Noodle', location: {lat: 22.370813, lng: 114.1357875}},
-	{localLanguage: '', title: 'Espuma', location: {lat: 22.299594, lng: 114.1723658}},
-	{localLanguage: '大勝軒 丸一', title: 'Taishoken Maruichi', location: {lat: 22.2865469, lng: 114.2164531}},
-	{localLanguage: '炑八韓烤', title: 'Meokbang Korean BBQ & Bar', location: {lat: 22.2799897, lng: 114.1820578}},
-	{localLanguage: '北京樓', title: 'Peking Garden Restaurant', location: {lat: 22.3577271, lng: 114.1264391}},
-	{localLanguage: '七輪燒肉', title: 'Yakiniku Shichirin', location: {lat: 22.2814449, lng: 114.1253643}},
-	{localLanguage: '', title: 'Beeger2', location: {lat: 22.2838502, lng: 114.1254129}}
+	{localLanguage: '雲南小鍋米線', title: 'Wannam Siu Wok Noodle', location: {lat: 22.370813, lng: 114.1357875}, ref: 0},
+	{localLanguage: '', title: 'Espuma', location: {lat: 22.299594, lng: 114.1723658}, ref: 1},
+	{localLanguage: '大勝軒 丸一', title: 'Taishoken Maruichi', location: {lat: 22.2865469, lng: 114.2164531}, ref: 2},
+	{localLanguage: '炑八韓烤', title: 'Meokbang Korean BBQ & Bar', location: {lat: 22.2799897, lng: 114.1820578}, ref: 3},
+	{localLanguage: '北京樓', title: 'Peking Garden Restaurant', location: {lat: 22.3577271, lng: 114.1264391}, ref: 4},
+	{localLanguage: '七輪燒肉', title: 'Yakiniku Shichirin', location: {lat: 22.2814449, lng: 114.1253643}, ref: 5},
+	{localLanguage: '', title: 'Beeger2', location: {lat: 22.2838502, lng: 114.1254129}, ref: 6}
 ]
 
 var map;
@@ -37,13 +37,14 @@ function initMap() {
 	for (var i = 0; i < initialPOI.length; i++) {
 		var position = initialPOI[i].location;
 		var title = initialPOI[i].title;
+		var id = initialPOI[i].ref
 
 		var marker = new google.maps.Marker({
 			map: map,
 			position: position,
 			title: title,
 			animation: google.maps.Animation.DROP,
-			id: i
+			id: id
 		})
 		//Push the marker to the markers array
 		markers.push(marker);
@@ -52,7 +53,9 @@ function initMap() {
 		// Create click event for each infowindow
 		marker.addListener('click', function(){
 			populateInfoWindow(this, largeInfoWindow);
-		})
+			toggleBounce(this);
+		});
+
 		map.fitBounds(bounds);
 	}
 
@@ -69,12 +72,13 @@ function initMap() {
 
 }
 
-// This populates the infowindow when the marker is clicked. Only one infowindow will open based on the marker position..
+// This populates the infowindow when the marker is clicked. Only one infowindow will open based on the marker position.
 function populateInfoWindow(marker, infowindow) {
 	if (infowindow.marker != marker) {
 		infowindow.marker = marker;
 		infowindow.setContent('<div>' + marker.title + '</div>');
 		infowindow.open(map, marker);
+//		infowindow.marker.setAnimation(google.maps.Animation.BOUNCE);
 		// Clear the marker property when the infowindow is closed
 		infowindow.addListener('closeclick', function(){
 			infowindow.setMarker(null);
@@ -82,17 +86,31 @@ function populateInfoWindow(marker, infowindow) {
 	}
 }
 
+// This function set the marker to bounce when it's clicked
+function toggleBounce(marker) {
+	console.log(marker.getAnimation());
+	if (marker.getAnimation() !== null) {
+		console.log(marker.getAnimation());
+		marker.setAnimation(null);
+	} else {
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		console.log('fucking bouncing!!!');
+	}
+};
+
 var model = function() {
 	this.showClickedInfoWindow = function (marker){
-		var position = marker.location;
-		var title = marker.title;
+/*		var position = marker.location;
+		var title = marker.title;*/
 		var infoWindow = new google.maps.InfoWindow();
-		var clickedMarker = new google.maps.Marker({
+/*		var clickedMarker = new google.maps.Marker({
 			map: map,
 			position: position,
 			title: title,
-		});
-		populateInfoWindow(clickedMarker, infoWindow);
+			animation: google.maps.Animation.BOUNCE
+		});*/
+		populateInfoWindow(marker, infoWindow);
+		toggleBounce(marker);
 	}
 }
 
@@ -105,10 +123,16 @@ var viewModel = function() {
 		self.poiList.push(poiItem);
 	});
 
+/*	self.currentPOI = ko.observableArray([]);
+
+	self.updateCurrentPOI = function(poi){
+		self.currentPOI() = poi;
+	}*/
+
 	self.showInfoWindow = function(poi){
-		console.log(this);
-		new model().showClickedInfoWindow(this);
-//		model().showClickedInfoWindow(this);
+		clickedMarkerRef = poi.ref;
+		console.log(clickedMarkerRef);
+		new model().showClickedInfoWindow(markers[clickedMarkerRef]);
 	};
 
 }
