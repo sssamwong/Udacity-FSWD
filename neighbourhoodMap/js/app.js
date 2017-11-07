43b111b16369d4898895532ef722a9dabad82c5a
Wong SS <siuswong6@gmail.com>
2017-11-08 00:53:57 +0800

    Adding the filter function to the sidebar search

--- neighbourhoodMap/js/app.js
+++ neighbourhoodMap/js/app.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
 // Default point of interests
 
 var initialPOI = [
-	{localLanguage: '雲南小鍋米線', title: 'Wannam Siu Wok Noodle', location: {lat: 22.370813, lng: 114.1357875}, ref: 0},
-	{localLanguage: '', title: 'Espuma', location: {lat: 22.299594, lng: 114.1723658}, ref: 1},
-	{localLanguage: '大勝軒 丸一', title: 'Taishoken Maruichi', location: {lat: 22.2865469, lng: 114.2164531}, ref: 2},
-	{localLanguage: '炑八韓烤', title: 'Meokbang Korean BBQ & Bar', location: {lat: 22.2799897, lng: 114.1820578}, ref: 3},
-	{localLanguage: '北京樓', title: 'Peking Garden Restaurant', location: {lat: 22.3577271, lng: 114.1264391}, ref: 4},
-	{localLanguage: '七輪燒肉', title: 'Yakiniku Shichirin', location: {lat: 22.2814449, lng: 114.1253643}, ref: 5},
-	{localLanguage: '', title: 'Beeger2', location: {lat: 22.2838502, lng: 114.1254129}, ref: 6},
-	{localLanguage: '', title: 'Solera Spanish Cuisine Concepts', location: {lat: 22.2967212, lng: 114.014821}, ref: 7}
+	{localLanguage: '雲南小鍋米線', title: 'Wannam Siu Wok Noodle', location: {lat: 22.370813, lng: 114.1357875}, ref: 0, display: true},
+	{localLanguage: '', title: 'Espuma', location: {lat: 22.299594, lng: 114.1723658}, ref: 1, display: true},
+	{localLanguage: '大勝軒 丸一', title: 'Taishoken Maruichi', location: {lat: 22.2865469, lng: 114.2164531}, ref: 2, display: true},
+	{localLanguage: '炑八韓烤', title: 'Meokbang Korean BBQ & Bar', location: {lat: 22.2799897, lng: 114.1820578}, ref: 3, display: true},
+	{localLanguage: '北京樓', title: 'Peking Garden Restaurant', location: {lat: 22.3577271, lng: 114.1264391}, ref: 4, display: true},
+	{localLanguage: '七輪燒肉', title: 'Yakiniku Shichirin', location: {lat: 22.2814449, lng: 114.1253643}, ref: 5, display: true},
+	{localLanguage: '', title: 'Beeger2', location: {lat: 22.2838502, lng: 114.1254129}, ref: 6, display: true},
+	{localLanguage: '', title: 'Solera Spanish Cuisine Concepts', location: {lat: 22.2967212, lng: 114.014821}, ref: 7, display: true}
 ]
 
 var map;
@@ -105,6 +105,7 @@ var viewModel = function() {
 	var self = this;
 
 	self.poiList = ko.observableArray([]);
+	self.userSearchKey = ko.observable('');
 
 	initialPOI.forEach(function(poiItem){
 		self.poiList.push(poiItem);
@@ -114,6 +115,13 @@ var viewModel = function() {
 		clickedMarkerRef = poi.ref;
 		new model().showClickedInfoWindow(markers[clickedMarkerRef]);
 	};
+
+	self.filterPOI = ko.computed(function() {
+		var search = self.userSearchKey().toLowerCase();
+		return ko.utils.arrayFilter(self.poiList(), function(poi){
+			return poi.title.toLowerCase().indexOf(search) >= 0;
+		});
+	});
 
 }
 
