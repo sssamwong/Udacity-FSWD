2e8cd1129f8c9617fd0e70cff8915cc409858f2b
Wong SS <siuswong6@gmail.com>
2017-11-20 00:56:19 +0800

    Adding Foursquare input into the infowindow

--- neighbourhoodMap/js/app.js
+++ neighbourhoodMap/js/app.js
@@ -9,14 +9,14 @@ $(function(){
 // Default point of interests
 
 var initialPOI = [
-	{title: 'Wannam Siu Wok Noodle', location: {lat: 22.370813, lng: 114.1357875}, ref: 0, url: true},
-	{title: 'Tsim Sha Tsui Espuma', location: {lat: 22.299594, lng: 114.1723658}, ref: 1, url: true},
-	{title: 'Taishoken Maruichi', location: {lat: 22.2865469, lng: 114.2164531}, ref: 2, url: true},
-	{title: 'Meokbang Korean BBQ & Bar', location: {lat: 22.2799897, lng: 114.1820578}, ref: 3, url: true},
-	{title: 'Kwai Fong Peking Garden Restaurant', location: {lat: 22.3577271, lng: 114.1264391}, ref: 4, url: true},
-	{title: 'Yakiniku Shichirin', location: {lat: 22.2814449, lng: 114.1253643}, ref: 5, url: true},
-	{title: 'Beeger2', location: {lat: 22.2838502, lng: 114.1254129}, ref: 6, url: true},
-	{title: 'Solera Spanish Cuisine Concepts', location: {lat: 22.2967212, lng: 114.014821}, ref: 7, url: true}
+	{title: 'Wannam Siu Wok Noodle', district: 'Kwai Chung', location: {lat: 22.370813, lng: 114.1357875}, ref: 0, url: true},
+	{title: 'Espuma', district: 'Tsim Sha Tsui', location: {lat: 22.299594, lng: 114.1723658}, ref: 1, url: true},
+	{title: 'Taishoken Maruichi', district: 'Taikoo', location: {lat: 22.2865469, lng: 114.2164531}, ref: 2, url: true},
+	{title: 'Meokbang Korean BBQ & Bar', district: 'Causeway Bay', location: {lat: 22.2799897, lng: 114.1820578}, ref: 3, url: true},
+	{title: 'Peking Garden Restaurant', district: 'Kwai Fong', location: {lat: 22.3577271, lng: 114.1264391}, ref: 4, url: true},
+	{title: 'Yakiniku Shichirin', district: 'Kennedy Town', location: {lat: 22.2814449, lng: 114.1253643}, ref: 5, url: true},
+	{title: 'Beeger2', district: 'Kennedy Town', location: {lat: 22.2838502, lng: 114.1254129}, ref: 6, url: true},
+	{title: 'Solera', district: 'Discovery Bay', location: {lat: 22.2967212, lng: 114.014821}, ref: 7, url: true}
 ]
 
 var map;
@@ -37,12 +37,14 @@ function initMap() {
 	for (var i = 0; i < initialPOI.length; i++) {
 		var position = initialPOI[i].location;
 		var title = initialPOI[i].title;
+		var district = initialPOI[i].district;
 		var id = initialPOI[i].ref;
 
 		var marker = new google.maps.Marker({
 			map: map,
 			position: position,
 			title: title,
+			district: district,
 			animation: google.maps.Animation.DROP,
 			id: id
 		})
@@ -62,8 +64,9 @@ function initMap() {
 function searchPOI(marker) {
 	var bounds = map.getBounds();
 	var placesService = new google.maps.places.PlacesService(map);
+	console.log(marker.title + marker.district);
 	placesService.textSearch({
-		query: marker.title,
+		query: marker.title + marker.district,
 		bounds: bounds
 	}, function(results, status){
 		if (status === google.maps.places.PlacesServiceStatus.OK){
@@ -89,7 +92,7 @@ function createMarkersForPlaces(places){
 		icon: icon,
 		title: place.name,
 		position: place.geometry.location,
-		id: place.place_id
+		id: place.place_id,
 	})
 	getPlacesDetails(marker, placeInfoWindow);
 }
@@ -97,6 +100,7 @@ function createMarkersForPlaces(places){
 // This is the place details search
 function getPlacesDetails(marker, infowindow) {
 	var service = new google.maps.places.PlacesService(map);
+	var comment = '';
 	service.getDetails({
 		placeId: marker.id
 	}, function(place, status){
@@ -108,12 +112,15 @@ function getPlacesDetails(marker, infowindow) {
 			}
 			if (place.photos) {
 				infoWindowHTML += '<br><br><img src="' + place.photos[0].getUrl(
-					{maxHeight: 150, maxWidth: 300}) + '">';
+					{maxHeight: 150, maxWidth: 300}) + '"><br>';
 			}
+			console.log(infoWindowHTML);
+			addingFoursquareAPI(marker, infoWindowHTML, infowindow)
+			console.log(infoWindowHTML)
+//			addingFoursquareAPI(marker, infoWindowHTML);
 			infoWindowHTML += '</div>';
-//			console.log('here' + marker.getPosition().lat());
-			infowindow.setContent(infoWindowHTML);
-			infowindow.open(map, marker);
+/*			infowindow.setContent(infoWindowHTML);
+			infowindow.open(map, marker);*/
 			infowindow.addListener('closeclick', function() {
 				infowindow.marker = null;
 			});
@@ -123,19 +130,25 @@ function getPlacesDetails(marker, infowindow) {
 
 // AJAX for the infowindow using foursquare API
 
-function addingFoursquareAPI (marker){
+function addingFoursquareAPI (marker, infoWindowHTML, infowindow){
 	var client_id= 'SQBOBMCCS1DFBV1OKHWLHZDOFEAM1IM3AJRFBJQVXZGW5FNP';
 	var client_secret= '2FXHOQ053U4F3ZIPMG442CVSFJD0ZJXZMG5J0KBLZQR3LFGH';
-	var requestURL = 'https://api.foursquare.com/v2/venues/explore?';
+	var requestURL = 'https://api.foursquare.com/v2/venues/explore?&intent=browse&';
 	var d = new Date();
 	var todayYYYYMMDD = d.getFullYear()+ '' + (d.getMonth()+1) + '' + d.getDate();
 	var place = marker.title;
+	var foursquareURL = requestURL + 'll=' + marker.getPosition().lat() + ',' + marker.getPosition().lng() + '&query=' + marker.title + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + todayYYYYMMDD + '&radius=250&limit=1';
+	var tips;
 
 	$.ajax({
 		dataType: 'jsonp',
-		url: requestURL + 'll=' + marker.getPosition().lat() + ',' + marker.getPosition().lng() + '&query=' + marker.title + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + todayYYYYMMDD,
+		url: foursquareURL,
 		success: function(data) {
-			console.log('hello');
+			tips = data.response.groups[0].items[0].tips[0].text;
+			infoWindowHTML += '<span class = "infowindow-width"> Review: ' + tips + '</span>';
+			infoWindowHTML += '</div>';
+			infowindow.setContent(infoWindowHTML);
+			infowindow.open(map, marker);
 		}
 	})
 };
@@ -156,7 +169,7 @@ function toggleBounce(marker) {
 function markerClickedHandler(marker){
 	searchPOI(marker);
 	toggleBounce(marker);
-	addingFoursquareAPI(marker);
+//	addingFoursquareAPI(marker);
 	map.setZoom(12);
 	map.setCenter(marker.getPosition());
 }
