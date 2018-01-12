43d4a2cafeb65afdd9a4bb07134af0d2f7546a31
Wong SS <siuswong6@gmail.com>
2018-01-12 17:49:23 +0800

    Adding the investment.html to show investments

--- Project_Item_Catalog_App/vagrant/project.py
+++ Project_Item_Catalog_App/vagrant/project.py
@@ -16,13 +16,17 @@ session = DBSession()
 @app.route('/catalog')
 def showCatalog():
 	catalog = session.query(Catalog).order_by(asc(Catalog.name))
-#	items = session.query(Investment).filter_by(catalog_id=catalog_id).all()
 #	if 'username' not in login_session:
 #		return render_template('publiccatalog.html', catalog=catalog)
 #	else:
 	return render_template('catalog.html', catalog=catalog)
 
-
+@app.route('/catalog/<int:catalog_id>/')
+@app.route('/catalog/<int:catalog_id>/investment/')
+def showInvestment(catalog_id):
+	catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+	investments = session.query(Investment).filter_by(catalog_id=catalog_id).all()
+	return render_template('investment.html', investments=investments, catalog=catalog)
 
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
