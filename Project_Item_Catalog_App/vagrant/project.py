4f465ad2daf65af8eb8955df9493fceb54976d55
Wong SS <siuswong6@gmail.com>
2018-01-30 00:46:37 +0800

    Adding functionality in project.py to obtain credentials of an existing user

--- Project_Item_Catalog_App/vagrant/project.py
+++ Project_Item_Catalog_App/vagrant/project.py
@@ -101,6 +101,12 @@ def gconnect():
 
 	print ('user name: ' + login_session['username'])
 
+	# Check if user exists, or create a new user
+	user_id = getUserID(login_session['email'])
+	if not user_id:
+		user_id = createUser(login_session)
+	login_session['user_id'] = user_id
+
 	output = ''
 	output += '<h1>Welcome '
 	output += login_session['username']
